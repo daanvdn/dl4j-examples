58c3cbec82552faba7333da6b04946a5efcdf584
Daan_Vandennest <Daan.VandenNest@nuance.com>
2019-04-09 14:01:18 +0200

    some changes to MnistMLPDistributedExample.java to make it more configurable from cli

--- dl4j-spark-examples/dl4j-spark/src/main/java/org/deeplearning4j/legacyExamples/mlp/MnistMLPDistributedExample.java
+++ dl4j-spark-examples/dl4j-spark/src/main/java/org/deeplearning4j/legacyExamples/mlp/MnistMLPDistributedExample.java
@@ -59,6 +59,15 @@ public class MnistMLPDistributedExample {
     @Parameter(names = "-numEpochs", description = "Number of epochs for training")
     private int numEpochs = 3;
 
+    @Parameter(names = "-networkMask", required = true)
+    private String networkMask;
+
+	@Parameter(names = "-controllerAddress", required = true)
+	private String controllerAddress;
+
+	@Parameter(names = "-workersPerNode", required = false)
+	private int workersPerNode = 4;
+
     public static void main(String[] args) throws Exception {
         new MnistMLPDistributedExample().entryPoint(args);
     }
@@ -127,12 +136,12 @@ public class MnistMLPDistributedExample {
             /**
              * if you're running this example on Hadoop/YARN, please provide proper netmask for out-of-spark comms
              */
-            .networkMask("10.0.0.0/16")
+            .networkMask(networkMask)
 
             /**
              * However, if you're running this example on Spark standalone cluster, you can rely on Spark internal addressing via $SPARK_PUBLIC_DNS env variables announced on each node
              */
-            .controllerAddress(useSparkLocal ? "127.0.0.1" : null)
+            .controllerAddress(useSparkLocal ? "127.0.0.1" : controllerAddress)
             .build();
 
         TrainingMaster tm = new SharedTrainingMaster.Builder(voidConfiguration, batchSizePerWorker)
@@ -142,7 +151,7 @@ public class MnistMLPDistributedExample {
             .batchSizePerWorker(batchSizePerWorker)
 
             // this option will enforce exactly 4 workers for each Spark node
-            .workersPerNode(4)
+            .workersPerNode(workersPerNode)
             .build();
 
         //Create the Spark network
@@ -150,15 +159,20 @@ public class MnistMLPDistributedExample {
 
         //Execute training:
         for (int i = 0; i < numEpochs; i++) {
-            sparkNet.fit(trainData);
-            log.info("Completed Epoch {}", i);
+            log.info("Started training epoch {}", i);
+        	sparkNet.fit(trainData);
+            log.info("Finished training epoch {}", i);
+            log.info("Started testing epoch {}", i);
+
+			//Perform evaluation (distributed)
+			//        Evaluation evaluation = sparkNet.evaluate(testData);
+			Evaluation evaluation = sparkNet.doEvaluation(testData, 64, new Evaluation(10))[0]; //Work-around for 0.9.1 bug: see https://deeplearning4j.org/releasenotes
+			log.info("***** Evaluation *****");
+			log.info(evaluation.stats());
+			log.info("Finished testing epoch {}", i);
         }
 
-        //Perform evaluation (distributed)
-//        Evaluation evaluation = sparkNet.evaluate(testData);
-        Evaluation evaluation = sparkNet.doEvaluation(testData, 64, new Evaluation(10))[0]; //Work-around for 0.9.1 bug: see https://deeplearning4j.org/releasenotes
-        log.info("***** Evaluation *****");
-        log.info(evaluation.stats());
+
 
         //Delete the temp training files, now that we are done with them
         tm.deleteTempFiles(sc);
